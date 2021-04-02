1bb79f8bd0a7a57e4c4dd141a0c7bcaa035e7c3f
wita <witalik1212@mail.ru>
2021-04-03 01:47:04 +0300

    implemented release_v0.4

---
+++ IntegruotuSistemuLaboratorinis3/IntegruotuSistemuLaboratorinis3/PerfMeasuringUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace IntegruotuSistemuLaboratorinis3
+{
+  class PerfMeasuringUtils
+  {
+    public static void GenerateFullAndSortStudents_List(int numOfstudents)
+    {
+      Stopwatch timer = new Stopwatch();
+      timer.Restart();
+
+      List<Student> students = StudentUtils.GenerateStudentsFullConstructor_List(numOfstudents);
+      StudentUtils.SortStudents_v0_4(students);
+
+      timer.Stop();
+
+      Console.WriteLine("Generation and dividing of random students by final score. " +
+        $"Time elapsed: {timer.Elapsed}, students amount {numOfstudents}");
+    }
+
+    public static void TestPerformance_v0_4()
+    {
+      Console.WriteLine("Full Constructor");
+      GenerateFullAndSortStudents_List(10_000);
+      GenerateFullAndSortStudents_List(100_000);
+      GenerateFullAndSortStudents_List(1_000_000);
+      GenerateFullAndSortStudents_List(10_000_000);
+    }
+  }
+}
