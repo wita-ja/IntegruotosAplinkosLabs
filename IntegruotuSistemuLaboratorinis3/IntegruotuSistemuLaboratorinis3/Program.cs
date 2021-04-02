937ddeebd10e56476a39eee9083d440d00d8f03f
wita <witalik1212@mail.ru>
2021-04-02 23:36:21 +0300

    implemented release_v0.3

--- IntegruotuSistemuLaboratorinis3/IntegruotuSistemuLaboratorinis3/Program.cs
+++ IntegruotuSistemuLaboratorinis3/IntegruotuSistemuLaboratorinis3/Program.cs
@@ -18,129 +18,43 @@ namespace IntegruotuAplinkuLaboratorinis3
         Console.WriteLine("\n       Menu\n" +
                             "1 - Add student.\n" +
                             "2 - Print students to table\n" +
-                            "3 - Import students from csv file" +
+                            "3 - Import students from csv file\n" +
                             "0 - Exit.\n");
 
         Console.Write("Choose which task to run: ");
         userChoice = Console.ReadLine();
-        switch (Convert.ToInt32(userChoice))
+        try
         {
-          case 0:
-            quit = true;
-            break;
-
-          case 1:
-            while (true)
-            {
-              Console.Write("Auto generate marks? y/n: ");
-
-              userChoice = Console.ReadLine();
-              if (userChoice == "y" || userChoice == "Y")
-              {
-                Console.Write("How many homeworks to generate: ");
-                userChoice = Console.ReadLine();
-
-                if (Convert.ToInt32(userChoice) > 0)
-                {
-                  students.Add(HandlingStudentInput(true, Convert.ToInt32(userChoice)));
-                  break;
-                }
-                else
-                {
-                  Console.WriteLine("The mark must be positive integer. Please try again");
-                  continue;
-                }
-              }
-              else if (userChoice == "n" || userChoice == "N")
-              {
-                students.Add(HandlingStudentInput(false));
-              }
+          switch (Convert.ToInt32(userChoice))
+          {
+            case 0:
+              quit = true;
               break;
-            }
-            break;
 
-          case 2:
-            Student.PrintInTable(students, true);
-            break;
-          case 3:
-            string mainPath = @Environment.CurrentDirectory;
-            students = Student.ImportStudentsFromCsv(mainPath + "\\students.csv");
-            Console.WriteLine("Students where successfully imported. Press any key to continue");
-            break;
-          default:
-            Console.WriteLine("Choice don't exists");
-            break;
+            case 1:
+              students.Add(StudentUtils.CreateStudentFromConsole());
+              break;
+
+            case 2:
+              StudentUtils.PrintInTable(students, true);
+              break;
+            case 3:
+              string mainPath = @Environment.CurrentDirectory;
+              students.AddRange(StudentUtils.ImportStudentsFromCsv(mainPath + "\\students.csv"));
+              Console.WriteLine("Students where successfully imported. Press any key to continue...");
+              break;
+            default:
+              Console.WriteLine("Choice don't exists");
+              break;
+          }
+
+        }
+        catch (FormatException e)
+        {
+          Console.WriteLine($"{e.Message} Menu option are called only using numbers. Press any key to continue...");
         }
         Console.ReadLine();
       }
-    }
-
-    static Student HandlingStudentInput(bool generateMarks, int homeworkCount = 0)
-    {
-      var newStudent = new Student();
-      var homeworks = new List<int>();
-      Console.Write("Input Student name: ");
-      newStudent.Name = Console.ReadLine();
-
-      Console.Write("Input Student surname: ");
-      newStudent.Surname = Console.ReadLine();
-
-      if (generateMarks)
-      {
-        Random random = new Random();
-        for (int i = 0; i < homeworkCount; i++) homeworks.Add(random.Next(1, 10));
-        homeworks.Sort();
-        newStudent.Homeworks = homeworks;
-        newStudent.ExamResult = random.Next(1, 10);
-      }
-      else
-      {
-        int userMarksInput;
-        while (true)
-        {
-          Console.Write("Enter Student homework marks(enter 0 to quit): ");
-          try
-          {
-            userMarksInput = Convert.ToInt32(Console.ReadLine());
-            if (userMarksInput == 0) break;
-            if (userMarksInput < 1 || userMarksInput > 10)
-            {
-              Console.WriteLine("The mark must be integer from 1-10 range. Please try again");
-              continue;
-            }
-            homeworks.Add(userMarksInput);
-          }
-          catch (Exception e)
-          {
-            Console.WriteLine($"{e.Message} Please try again.");
-            continue;
-          }
-        }
-        newStudent.Homeworks = homeworks;
-
-        while (true)
-        {
-          Console.Write("Enter Student exam mark: ");
-          try
-          {
-            userMarksInput = Convert.ToInt32(Console.ReadLine());
-            if (userMarksInput < 1 || userMarksInput > 10)
-            {
-              Console.WriteLine("The mark must be integer from 1-10 range. Please try again");
-              continue;
-            }
-            newStudent.ExamResult = userMarksInput;
-          }
-          catch (Exception e)
-          {
-            Console.WriteLine($"{e.Message} Please try again.");
-            continue;
-          }
-          break;
-        }
-      }
-
-      return newStudent;
     }
   }
 }
--- IntegruotuSistemuLaboratorinis3/IntegruotuSistemuLaboratorinis3/Student.cs
+++ IntegruotuSistemuLaboratorinis3/IntegruotuSistemuLaboratorinis3/Student.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Collections.Generic;
-using System.IO;
+using System;
 
 namespace IntegruotuSistemuLaboratorinis3
 {
@@ -28,13 +27,14 @@ namespace IntegruotuSistemuLaboratorinis3
 
     public double CalcFinalPointsUsingAvg()
     {
-      return ((Homeworks.Average() * 0.3) + ExamResult * 0.7);
+      double average = Homeworks.Average();
+      double finalPoints = (average * 0.3) + (ExamResult * 0.7);
+      return finalPoints;
     }
 
     public double CalcFinalPointsUsingMedian()
     {
-      double homeworkMedian;
-
+      double homeworkMedian, finalPoints;
       int numberCount = Homeworks.Count();
       int halfIndex = Homeworks.Count() / 2;
       var sortedNumbers = Homeworks.OrderBy(n => n);
@@ -48,74 +48,7 @@ namespace IntegruotuSistemuLaboratorinis3
       {
         homeworkMedian = sortedNumbers.ElementAt(halfIndex);
       }
-
-      return ((homeworkMedian * 0.3) + ExamResult * 0.7);
-    }
-
-    public static void PrintInTable(List<Student> students, bool median)
-    {
-      students = students.OrderBy(student => student.Name).ToList();
-      string line = "----------------------------------------------------------------------------------------------";
-
-      Console.Write('\n');
-      if (median == false)
-      {
-        Console.WriteLine("{0, -30} {1, -20} {2, -5}", "Surname", "Name", "Final points (Avg.)");
-        Console.Write(line);
-        Console.Write('\n');
-
-        students.ForEach(student =>
-        {
-          Console.WriteLine("{0, -30} {1, -20} {2, -5:#.##}", student.Surname, student.Name, student.CalcFinalPointsUsingAvg());
-        });
-      }
-      else
-      {
-        Console.WriteLine("{0, -30} {1, -20} {2, -5}", "Surname", "Name", "Final points (Avg.) / Final points (Med.)");
-        Console.Write(line);
-        Console.Write('\n');
-
-        students.ForEach(student =>
-        {
-          Console.WriteLine("{0, -30} {1, -20} {2, -21:#.##} {3, -20:#.##}", student.Surname, student.Name, student.CalcFinalPointsUsingAvg(), student.CalcFinalPointsUsingMedian());
-        });
-      }
-    }
-
-    public static List<Student> ImportStudentsFromCsv(string fileName)
-    {
-      List<Student> students = new List<Student>();
-      try
-      {
-        students = File.ReadAllLines(fileName)
-                .Skip(1)
-                .Select(line => line.Split(","))
-                .Select(values => StudentFromCsvString(values))
-                .ToList();
-      }
-      catch (FileNotFoundException e)
-      {
-        Console.WriteLine(e.ToString());
-      }
-      return students;
-    }
-
-    private static Student StudentFromCsvString(string[] values)
-    {
-      List<int> homeworks = new List<int>();
-
-      for (int i = 2; i <= values.Length - 1; i++)
-      {
-        int currval = Convert.ToInt32(values[i]);
-        if (currval < 1 || currval > 10)
-        {
-          throw new ArgumentOutOfRangeException(nameof(currval), $"Integer is out of allowed range (1-10).");
-        }
-        else homeworks.Add(currval);
-      }
-      return new Student(values[0], values[1], homeworks, Convert.ToInt32(values[7]));
+      return finalPoints = (homeworkMedian * 0.3) + (ExamResult * 0.7);
     }
   }
 }
-
-
